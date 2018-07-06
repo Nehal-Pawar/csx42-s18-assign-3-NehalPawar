89699ce4a187463bd468d10a3934c7569014c835
Nehal Pawar <npawar3@binghamton.edu>
2018-07-05 23:08:21 -0400

    First commit

---
+++ NehalRajendra_Pawar_assign_3/studentCoursesBackup/src/studentCoursesBackup/util/FileProcessor.java
@@ -0,0 +1,51 @@
+package fourWayStreetLights.util;
+
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+
+public class FileProcessor
+{
+    FileReader F = null;
+    BufferedReader BR = null;
+    List<String> zoom = new ArrayList<>();
+    public List<String> openFile(String InputFileName)
+    {
+        try
+        {
+            F = new FileReader(InputFileName);
+            BR = new BufferedReader(F);
+            String number = "";
+            while (number != null)
+            {
+                number = readLine();
+                if (number == null) break;
+                zoom.add(number);
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+		MyLogger.writeMessage("File not found at location: " + InputFileName, MyLogger.DebugLevel.FILE_PROCESSOR);
+            System.err.println("File not found at location: " + InputFileName);
+            System.exit(1);
+        }
+        return zoom;
+    }
+    public String readLine()
+    {
+        String line = null;
+        try
+        {
+            line = BR.readLine();
+        }
+        catch (Exception e)
+        {
+            System.err.println("Error while reading file");
+		MyLogger.writeMessage("Error while reading file" , MyLogger.DebugLevel.FILE_PROCESSOR);
+
+            System.exit(1);
+        }
+        return line;
+    }
+}
---
+++ NehalRajendra_Pawar_assign_3/studentCoursesBackup/src/studentCoursesBackup/util/Results.java
@@ -0,0 +1,50 @@
+package fourWayStreetLights.util;
+
+import java.util.List;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.Vector;
+import fourWayStreetLights.util.MyLogger;
+
+public class Results  implements FileDisplayInterface, StdoutDisplayInterface{
+
+	public static List<String> TestResult = new Vector<String>();
+String result = "";
+/*wriet to output file the list that stores result*/
+public void writeToFile(String fileName)
+{
+    // FileOutputStream outputStream;
+    try
+    {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+        for (String str : TestResult)
+        {
+            //byte[] strToBytes = str.getBytes();
+            //outputStream.write(strToBytes);
+            writer.write(str);
+            writer.write("\n");
+        }
+        //outputStream.close();
+        writer.close();
+    }
+    catch (Exception e)
+    {
+        System.out.println("Exception while writing to file." + e.toString());
+        System.exit(1);
+    }
+}
+/* print to console */
+public void writeToStdout(String s)
+{
+    //for(String str : TestResult )
+    System.out.println(s);
+}
+/* store and display result from all tests*/
+public static void storeNewResult(String result)
+{
+    TestResult.add(result);
+    MyLogger.writeMessage(result, MyLogger.DebugLevel.AllStates);
+    //writeToStdout(result);
+}
+		
+}
